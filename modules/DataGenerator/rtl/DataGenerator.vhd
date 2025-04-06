8877226ae058029e24e1167fa531322e155bf68c
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-04-06 02:23:06 +0200

    Updated Generator
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- modules/DataGenerator/rtl/DataGenerator.vhd
+++ modules/DataGenerator/rtl/DataGenerator.vhd
@@ -23,6 +23,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 use IEEE.NUMERIC_STD.ALL;
+use work.Axi4Pkg.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -31,16 +32,14 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity DataGenerator is
     Generic(
-        MAX_VAL_G    : natural := 1024;
-        MIN_VAL_G    : natural := 0;
-        DATA_WIDTH_G : natural := 10
+        MAX_VAL_G : natural := 1024;
+        MIN_VAL_G : natural := 0
     );
     Port (
-        clk_i   : in  STD_LOGIC;
-        rst_i   : in  STD_LOGIC;
-        data_o  : out STD_LOGIC_VECTOR(DATA_WIDTH_G-1 downto 0);
-        valid_o : out STD_LOGIC;
-        ready_i : in  STD_LOGIC
+        clk_i     : in  STD_LOGIC;
+        rst_i     : in  STD_LOGIC;
+        axisSrc_o : out Axi4StreamSource;
+        axisDst_i : in  Axi4StreamDestination
     );
 end DataGenerator;
 
@@ -52,19 +51,27 @@ architecture Behavioral of DataGenerator is
         );
 
     type RegType is record
-        counter : unsigned(DATA_WIDTH_G-1 downto 0);
-        state   : StateType;
-        valid   : STD_LOGIC;
-        upDown  : STD_LOGIC;
+        state        : StateType;
+        axiSrcDriver : axisSrc_o'subtype;
+        upDown       : STD_LOGIC;
     end record RegType;
 
-    constant REG_TYPE_INIT_C : RegType := (
-            counter => to_unsigned(MIN_VAL_G, DATA_WIDTH_G),
-            state   => ADVANCE_S,
-            valid   => '0',
-            upDown  => '0'
+    constant AXI_4_STREAM_SRC_INIT_C : axisSrc_o'subtype := (
+            TVALID  => '0',
+            TDATA   => (others => '0'),
+            TSTRB   => (others => '1'),
+            TKEEP   => (others => '1'),
+            TLAST   => '0',
+            TID     => (others => '0'),
+            TDEST   => (others => '0'),
+            TWAKEUP => '0'
         );
 
+    constant REG_TYPE_INIT_C : RegType := (
+            state        => ADVANCE_S,
+            axiSrcDriver => AXI_4_STREAM_SRC_INIT_C,
+            upDown       => '0'
+        );
     signal r   : RegType;
     signal rin : RegType;
 
@@ -77,28 +84,28 @@ begin
         case r.state is
             when ADVANCE_S =>
                 if (r.upDown = '0') then
-                    if (r.counter = MAX_VAL_G-1) then
-                        v.upDown  := '1';
-                        v.counter := r.counter - 1;
+                    if (to_integer(unsigned(r.axiSrcDriver.tdata)) = MAX_VAL_G-1) then
+                        v.upDown             := '1';
+                        v.axiSrcDriver.tdata := STD_LOGIC_VECTOR(unsigned(r.axiSrcDriver.tdata) - 1);
                     else
-                        v.counter := r.counter + 1;
+                        v.axiSrcDriver.tdata := STD_LOGIC_VECTOR(unsigned(r.axiSrcDriver.tdata) + 1);
                     end if;
                 else
-                    if (r.counter = MIN_VAL_G) then
-                        v.upDown  := '0';
-                        v.counter := r.counter + 1;
+                    if (to_integer(unsigned(r.axiSrcDriver.tdata)) = MIN_VAL_G) then
+                        v.upDown             := '0';
+                        v.axiSrcDriver.tdata := STD_LOGIC_VECTOR(unsigned(r.axiSrcDriver.tdata) + 1);
                     else
-                        v.counter := r.counter - 1;
+                        v.axiSrcDriver.tdata := STD_LOGIC_VECTOR(unsigned(r.axiSrcDriver.tdata) - 1);
                     end if;
                 end if;
 
-                v.valid := '1';
-                v.state := HANDSHAKE_S;
+                v.axiSrcDriver.tvalid := '1';
+                v.state               := HANDSHAKE_S;
 
             when HANDSHAKE_S =>
-                if (ready_i = '1') then
-                    v.valid := '0';
-                    v.state := ADVANCE_S;
+                if (axisDst_i.tready = '1') then
+                    v.axiSrcDriver.tvalid := '0';
+                    v.state               := ADVANCE_S;
                 end if;
 
             when others =>
@@ -111,8 +118,15 @@ begin
 
         rin <= v;
 
-        valid_o <= r.valid;
-        data_o  <= std_logic_vector(r.counter);
+        -- Drive outputs
+        axisSrc_o.tvalid  <= r.axiSrcDriver.tvalid;
+        axisSrc_o.tdata   <= r.axiSrcDriver.tdata;
+        axisSrc_o.tstrb   <= r.axiSrcDriver.tstrb;
+        axisSrc_o.tkeep   <= r.axiSrcDriver.tkeep;
+        axisSrc_o.tlast   <= r.axiSrcDriver.tlast;
+        axisSrc_o.tid     <= r.axiSrcDriver.tid;
+        axisSrc_o.tdest   <= r.axiSrcDriver.tdest;
+        axisSrc_o.twakeup <= r.axiSrcDriver.twakeup;
     end process p_Comb;
 
     p_Seq : process (clk_i)
--- modules/DataGenerator/tb/DataGeneratorTb.vhd
+++ modules/DataGenerator/tb/DataGeneratorTb.vhd
@@ -30,24 +30,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
+use work.Axi4Pkg.all;
 
 entity DataGeneratorTb is
 --  Port ( );
 end DataGeneratorTb;
 
 architecture Behavioral of DataGeneratorTb is
-	constant MAX_VAL_C    : natural := 1024;
-	constant MIN_VAL_C    : natural := 0;
-	constant DATA_WIDTH_C : natural := 10;
+	constant MAX_VAL_C : natural := 1024;
+	constant MIN_VAL_C : natural := 0;
+
+
+	constant AXI_DATA_WIDTH_C : natural := 16;
 
 	constant CLK_PERIOD_C : time := 33 ns;
 
 	-- UUT signals
-	signal clk_i   : STD_LOGIC;
-	signal rst_i   : STD_LOGIC;
-	signal data_o  : STD_LOGIC_VECTOR(DATA_WIDTH_C-1 downto 0);
-	signal valid_o : STD_LOGIC;
-	signal ready_i : STD_LOGIC;
+	signal clk_i     : STD_LOGIC;
+	signal rst_i     : STD_LOGIC;
+	signal axisSrc_o : Axi4StreamSource(
+		TDATA(AXI_DATA_WIDTH_C-1 downto 0),
+		TSTRB(AXI_DATA_WIDTH_C/8-1 downto 0),
+		TKEEP(AXI_DATA_WIDTH_C/8-1 downto 0),
+		TID(1-1 downto 0),
+		TDEST(1-1 downto 0)
+	);
+	signal axisDst_i : Axi4StreamDestination;
 begin
 
 	clock : process
@@ -62,22 +70,21 @@ begin
 	DataGenerator_1 : entity work.DataGenerator
 		generic map (
 			MAX_VAL_G => MAX_VAL_C,
-			MIN_VAL_G => MIN_VAL_C,
-			DATA_WIDTH_G => DATA_WIDTH_C
+			MIN_VAL_G => MIN_VAL_C
 		)
 		port map (
-			clk_i   => clk_i,
-			rst_i   => rst_i,
-			data_o  => data_o,
-			valid_o => valid_o,
-			ready_i => ready_i
+			clk_i     => clk_i,
+			rst_i     => rst_i,
+			axisSrc_o => axisSrc_o,
+			axisDst_i => axisDst_i
 		);
+
 	stimulus : process
 	begin
 		rst_i <= '1';
 		-- initialize signals
 
-		ready_i <= '0';
+		axisDst_i.tready <= '0';
 
 		wait for CLK_PERIOD_C*3;
 
@@ -90,14 +97,14 @@ begin
 		-- stimulus
 		for i in 0 to 2048 loop
 			wait for CLK_PERIOD_C*4;
-			ready_i <= '1';
-			if (valid_o = '1') then
+			axisDst_i.tready <= '1';
+			if (axisSrc_o.tvalid = '1') then
 				wait for CLK_PERIOD_C;
 			else
-				wait until valid_o = '1';
+				wait until axisSrc_o.tvalid = '1';
 			end if;
 
-			ready_i <= '0' ;
+			axisDst_i.tready <= '0' ;
 		end loop ;
 
 		wait;
