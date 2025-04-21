f0045bba60c29d752de3fb8dd98387f6e1cda53e
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-04-21 22:06:03 +0200

    Reduce buffer size, increase readout to speedup axi
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- modules/DataBuffer/rtl/BramBufferPkg.vhd
+++ modules/DataBuffer/rtl/BramBufferPkg.vhd
@@ -8,7 +8,7 @@ use IEEE.math_real."log2";
 
 package BramBufferPkg is
 
-	constant BRAM_BUFFER_NUM_ADDRESSES_C     : natural := 1024 * 8;
+	constant BRAM_BUFFER_NUM_ADDRESSES_C     : natural := 1024 * 4;
 	constant BRAM_BUFFER_PACKING_C           : natural := 2;
 	constant BRAM_BUFFER_SAMPLE_DATA_WIDTH_C : natural := 12;
 	constant BRAM_BUFFER_DATA_WIDTH_C        : natural := BRAM_BUFFER_PACKING_C * BRAM_BUFFER_SAMPLE_DATA_WIDTH_C;
@@ -16,7 +16,7 @@ package BramBufferPkg is
 	constant BRAM_BUFFER_LATENCY_C           : natural := 1;
 	constant BRAM_BUFFER_MEMORY_SIZE_C       : natural := BRAM_BUFFER_NUM_ADDRESSES_C * BRAM_BUFFER_DATA_WIDTH_C;
 	constant BRAM_BUFFER_ADDR_WIDTH_C        : natural := natural(ceil(log2(real(BRAM_BUFFER_NUM_ADDRESSES_C))));
-	constant BRAM_BUFFER_MAX_LENGTH_C        : natural := 2;
+	constant BRAM_BUFFER_MAX_LENGTH_C        : natural := 8;
 	constant BRAM_BUFFER_LENGTH_WIDTH_C      : natural := natural(ceil(log2(real(BRAM_BUFFER_MAX_LENGTH_C))));
 	
 	type TmpBufferArray is array (natural range <>) of STD_LOGIC_VECTOR;
